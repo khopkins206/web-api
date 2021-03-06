1780191a2644e4e86fb153eef7af9a7b174e5e98
Katherine Jackson <katherine.jackson206@gmail.com>
2021-03-05 22:17:15 -0600

    changed my JS and html... added CSS

--- js/style.js
+++ js/style.js
@@ -1,88 +1,154 @@
 // create variables to connect to html
-var startBtn = document.querySelector("start-button");
-var quizContainer = document.querySelector("quiz-questions");
-var quizResults = document.querySelector("quiz-results");
-
-// get start button to initiate quiz
-startBtn.addEventListener("click", quizContainer);
-
-// create quiz
-function buildQuiz()
-// function showResults()
-
-// quiz questions and answers 
-var quizQuestion = [
-  {
-    question: "What is my name?",
-    answers: {
-      A: "Katie",
-      B: "Sally",
-      C: "June",
-      D: "Jenny"
+var questions = [{
+        title: "Which of the following funcJavaScript is a ___ -side programming language?",
+        choices: ["Client", "Server", "Both", "None"],
+        answer: "Both"
     },
-    correctAnswer: "A"
+    {
+        title: "Which are the correct "if" statements to execute certain code if “x” is equal to 10?",
+        choices: ["if(x 10)", "if(x = 10)", "if(x == 10)", "if(x != 10)"],
+        answer: "if(x == 10)"
     },
-  {
-    question: "My age is?",
-    answers: {
-      A: "11",
-      B: "24",
-      C: "38",
-      D: "28"
+    {
+        title: "What is the correct JavaScript syntax to print “DataFlair” in the console?",
+        choices: ["print(“DataFlair”);", "console.print(“DataFlair”);", "log.console(“DataFlair”);", "console.log(“DataFlair”);"],
+        answer: "console.log(“DataFlair”);"
     },
-    correctAnswer: "D"
-  },
-  {
-    question: "What is my favorite color?",
-    answers: {
-      A: "Pink",
-      B: "Yellow",
-      C: "Green",
-      D: "Orange"
+    {
+        title: "Which of the following function of an array object adds and/or removes elements from an array?",
+        choices: ["toSource( )", "sort( )", "unshift( )", "splice( )"],
+        answer: "splice( )"
     },
-    correctAnswer: "B"
-  },
-  {
-    question: "What is my favorite food?",
-    answers: {
-      A: "Pizza",
-      B: "Burger",
-      C: "Noodles",
-      D: "Pasta"
-    },
-    correctAnswer: "A"
+    {
+        title: "Which of the following statements will show a message as well as ask for user input in a popup?",
+        choices: ["alert()", "prompt()", " confirm()", "message()"],
+        answer: "prompt( )"
     }
 ]
-console.log(quizQuestion);
+
+//setting the numerical variables for the functions.. scores and timers.. 
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
+
+//starts the countdown timer once user clicks the 'start' button
+function start() {
+
+    timeLeft = 75;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    timer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
+        //proceed to end the game function when timer is below 0 at any time
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame(); 
+        }
+    }, 1000);
+
+    next();
+}
+
+//stop the timer to end the game 
+function endGame() {
+    clearInterval(timer);
+
+    var quizContent = `
+    <h2>Game over!</h2>
+    <h3>You got a ` + score +  ` /100!</h3>
+    <h3>That means you got ` + score / 20 +  ` questions correct!</h3>
+    <input type="text" id="name" placeholder="First name"> 
+    <button onclick="setScore()">Set score!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//store the scores on local storage
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName",  document.getElementById('name').value);
+    getScore();
+}
 
 
-// build quiz
-function buildQuiz() {
-  //variable to store HTML answer
-  var output = [],
-
-    quizQuestion.forEach: (
-      (currentQuestion, questionNum) => {
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
     
-      //variable to store possible answers
-      var answer = [];
-
-      for (letter in currentQuestion.answer) {
-
-        answer.push(
-          ` <label>
-          <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answer[letter]}
-        </label>`
-        );
-      }
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
     
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
+    `;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//clears the score name and value in the local storage if the user selects 'clear score'
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName",  "");
+
+    resetGame();
+}
+
+//reset the game 
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    var quizContent = `
+    <h1>
+        JavaScript Quiz!
+    </h1>
+    <h3>
+        Click to play!   
+    </h3>
+    <button onclick="start()">Start!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//deduct 15seconds from the timer if user chooses an incorrect answer
+function incorrect() {
+    timeLeft -= 10; 
+    next();
+}
+
+//increases the score by 20points if the user chooses the correct answer
+function correct() {
+    score += 15;
+    next();
+}
+
+//loops through the questions 
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
     }
-  
-  quizContainer.innerHTML = output.join("")
+
+    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+            buttonCode = buttonCode.replace("[ANS]", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+        quizContent += buttonCode
+    }
+
+
+    document.getElementById("quizBody").innerHTML = quizContent;
 }
